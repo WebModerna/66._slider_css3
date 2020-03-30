ef6141671cf0e67049bc4a86221ad95def5da256
WebModerna <edgardogalletto@gmail.com>
2020-03-30 13:49:19 -0300

    Mejora en la definición de las variables que toman las animaciones de ingreso y salida.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ window.addEventListener("load", iniciadora);
 
 function iniciadora()
 {
-	// El arreglo con cada uno de los sliders.
+	// Animación de entrada y salida
+	let animacionEntrada = "slideInRight",
+		animacionSalida = "slideOutLeft";
 
+	// El arreglo con cada uno de los sliders.
 	const sliders = document.querySelectorAll(".animacion__carrousel");
+
+
+	/* // Para inicializar los sliders ocultos
+	for( let e = 0; e < sliders.length; e++ )
+	{
+		sliders[e].classList.add(animacionSalida);
+		console.log(sliders[e]);
+	} */
 
 	// Función que se autoejecuta cada 3 segundos
 	let i = 0;
@@ -17,20 +28,21 @@ function iniciadora()
 		// Control del contador
 		i = ( i + 1 ) % sliders.length;
 		der();
+		console.log(sliders[i]);
 	}
 	temporizadorInfinito();
 	
 	// Desplazamiento hacia la izquierda y desaparece
 	function izq()
 	{
-		sliders[i].classList.add("slideOutLeft");
-		sliders[i].classList.remove("slideInRight");
+		sliders[i].classList.add(animacionSalida);
+		sliders[i].classList.remove(animacionEntrada);
 	}
 
 	// Desplazamiento desde la derecha y se queda en el centro 
 	function der()
 	{
-		sliders[i].classList.add("slideInRight");
-		sliders[i].classList.remove("slideOutLeft");
+		sliders[i].classList.add(animacionEntrada);
+		sliders[i].classList.remove(animacionSalida);
 	}
 }
